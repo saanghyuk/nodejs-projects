aad98cc2ee4d3c7f8898e57b5972536d845c5f82
Sanghyuk <saanghyuk@gmail.com>
2017-12-14 09:17:54 +0900

    simle http

---
+++ simple-server/index.js
@@ -0,0 +1,50 @@
+const http = require('http'); //http module import
+const url=require('url');
+const path = require('path');
+const fs = require('fs');
+
+const mimeTypes = {
+    "html": "text/html",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpg",
+    "png": "image/png",
+    "js": "text/javascript",
+    "css": "text/css"
+};
+
+http.createServer((req, res)=>{
+   var uri= url.parse(req.url).pathname;
+   var fileName = path.join(process.cwd(), uri);
+   console.log(`Loading ${uri}`);
+   var stats;
+
+   try{
+       stats = fs.lstatSync(fileName);
+   } catch(e){
+        res.writeHead(404, {'Content-type': 'text/plain'});
+        res.write('404 Not Found\n');
+        res.end();
+        return;
+   }
+
+   if(stats.isFile()){
+       var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
+       res.writeHead(200, {'Content-type': mimeType});
+
+        var fileStream = fs.createReadStream(fileName);
+        fileStream.pipe(res);
+
+   }else if(stats.isDirectory()){
+       res.writeHead(302, {
+           'Location': 'index.html'
+       });
+       res.end();
+   }else{
+       res.writeHead(500, {
+           'Content-type': 'text/plain'
+       });
+        res.write('500 Interval Error \n');
+        res.end();
+   }
+
+}).listen(1337);
