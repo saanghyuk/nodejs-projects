5db0983bd872025369b85d4ae83618111571d642
Sanghyuk <saanghyuk@gmail.com>
2017-12-19 17:12:54 +0900

    node-blog

--- node-blog-system/routes/categories.js
+++ node-blog-system/routes/categories.js
@@ -4,54 +4,50 @@ var mongo = require('mongodb');
 var db = require('monk')('localhost/nodeblog');
 
 router.get('/show/:category', function(req, res, next) {
-	var posts = db.get('posts');
+    var posts = db.get('posts');
 
-	posts.find({category: req.params.category}, {}, (err, posts)=>{
-		res.render('index', {
-			'title': req.params.category,
-			'posts': posts
-		});
-	});
+    posts.find({category: req.params.category},{},function(err, posts){
+        res.render('index',{
+            'title': req.params.category,
+            'posts': posts
+        });
+    });
 });
 
-/* GET posts listing. */
 router.get('/add', function(req, res, next) {
-	res.render('addcategory', {
-		title: 'Add Category'
-	});
+    res.render('addcategory',{
+        'title': 'Add Category'
+    });
 });
 
 router.post('/add', function(req, res, next) {
-	//Get Form values
-	var name = req.body.name;
+    // Get Form Values
+    var name = req.body.name;
 
-	//Form Validation
-	req.checkBody('name', 'name field is required').notEmpty();
+    // Form Validation
+    req.checkBody('name','Name field is required').notEmpty();
 
-	//CHeck Validation Errors
-	var errors = req.validationErrors();
+    // Check Errors
+    var errors = req.validationErrors();
 
-	if (errors) {
-		res.render('addcategory', {
-			errors: errors
-		});
-	} else {
-		var categories = db.get('categories');
-		categories.insert(
-			{
-				name
-			},
-			(err, category) => {
-				if (err) {
-					res.send(err);
-				} else {
-					req.flash('success', 'Add Category');
-					res.location('/');
-					res.redirect('/');
-				}
-			}
-		);
-	}
+    if(errors){
+        res.render('addpost',{
+            "errors": errors
+        });
+    } else {
+        var categories = db.get('categories');
+        categories.insert({
+            "name": name,
+        }, function(err, post){
+            if(err){
+                res.send(err);
+            } else {
+                req.flash('success','Category Added');
+                res.location('/');
+                res.redirect('/');
+            }
+        });
+    }
 });
 
 module.exports = router;
